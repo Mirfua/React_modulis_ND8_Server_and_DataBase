b903a8cbe517c9bb13b8031f5dec47f79f2252f7
Jevgenijus <j.suchodolskis@gmail.com>
2021-10-30 00:27:26 +0300

    Add, edit & remove animals

--- my-app/src/Components/ZooAnimal.jsx
+++ my-app/src/Components/ZooAnimal.jsx
@@ -1,5 +1,9 @@
 
-function ZooAnimal({animal}) {
+function ZooAnimal({animal, modal}) {
+
+    const  showEdit = () => {
+        modal(animal)
+    }
 
     return (
         <div className="zoo__list__animal">
@@ -9,6 +13,7 @@ function ZooAnimal({animal}) {
                 <span><i>Born date:</i> {new Date(animal.born).getFullYear()}</span>
                 <span><i>Weight:</i> {animal.weight} kg.</span>
             </div>
+            <button onClick={showEdit}>Edit</button>
         </div>
     )
 }
